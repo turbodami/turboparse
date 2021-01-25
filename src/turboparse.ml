7445f642365720d87b9e1bc2b3d8b4fe8cd31151
Damiano Presa <presadamiano@gmail.com>
2021-01-25 12:15:57 +0100

    first commit

---
+++ src/turboparse.ml
@@ -0,0 +1,19 @@
+type input =
+    {
+        text: string;
+        pos: int
+    }
+
+let make_input (s: string): input =
+    { text = s; pos = 0 }
+
+type error = 
+    {
+        desc: string;
+        pos: int
+    }
+
+type parser = 
+    {
+        run: input -> (input * 'a, error) result
+    }
