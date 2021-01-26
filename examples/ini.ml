0a4a1c4acf3d7cf6fe712847e95193430fcf3833
Damiano Presa <presadamiano@gmail.com>
2021-01-26 22:35:03 +0100

    ini.ml completed

--- examples/ini.ml
+++ examples/ini.ml
@@ -1,54 +1,71 @@
-type key = string
-type value = string
-type pair = key * value
-type section = 
-    {
-        name: string;
-        pairs: pair list
-    }
+open Parcoom
 
-let show_pair (p: pair): string =
-    Printf.sprintf "(%s, %s)" key value 
+type key_t = string
+type value_t = string
+type pair_t = key_t * value_t
+type section_t =
+  { name: string;
+    pairs: pair_t list;
+  }
 
-let show_pairs (pair: list): string =
-    pairs
-    |> List.map show_pair
-    |> String.concat ","
-    |> Printf.sprintf "[%s]"
+let show_pair ((key, value): pair_t): string =
+  Printf.sprintf "(%s, %s)" key value
 
-let show_section (sec: section): string =
-    Printf.sprintf "{ name = %s; pairs = %s }" 
-    sec.name 
-    (show_pairs sec.pairs)
+let show_pairs (pairs: pair_t list): string =
+  pairs
+  |> List.map show_pair
+  |> String.concat ","
+  |> Printf.sprintf "[%s]"
 
-let show_sections (sections: section list): string = 
-    sections
-    |> List.map show_sections
-    |> String.concat ","
-    |> Printf.printf "[%s]"
+let show_section (section: section_t): string =
+  Printf.sprintf
+    "{ name = %s; pairs = %s }"
+    section.name
+    (show_pairs section.pairs)
 
-let ini: section list Turboparse.parser = 
-    Turboparse.failwith
-        { desc = "Not implemented yet";
-          pos = 0;  
-        }
+let show_sections (sections: section_t list): string =
+  sections
+  |> List.map show_section
+  |> String.concat ","
+  |> Printf.sprintf "[%s]"
 
-let read_whole_file (file_path: string): string = 
-    let ch = open_in file_path in
-    let n = in_channel_length ch in
-    let s = really_input_string ch n
-    close_in ch;
-    s
+let read_whole_file (file_path: string): string =
+  let ch = open_in file_path in
+  let n = in_channel_length ch in
+  let s = really_input_string ch n in
+  close_in ch;
+  s
 
-let () = 
-    let result = "./test.ini"
-                 |> read_whole_file
-                 |> Turboparse.make_input
-                 |> ini.run
-    in
-    match result with
-    | Ok (_, sections) -> sections |> show_sections |> print_endline
-    | Error error -> Printf.printf 
-                        "Error happened at %d: %s\n" 
-                        error.pos 
-                        error.desc
+let section_name: string Parcoom.parser =
+  prefix "[" *> parse_while (fun x -> x != ']') <* any_char
+
+let is_space (x: char) = x == ' ' || x == '\n'
+
+let wss: string Parcoom.parser =
+  parse_while is_space
+
+let pair: pair_t Parcoom.parser =
+  let name = parse_while (fun x -> not (is_space x) && x != '=') in
+  (wss *> name <* wss <* prefix "=" <* wss) <*> (name <* wss)
+
+let section: section_t Parcoom.parser =
+  section_name <*> many pair
+  |> map (fun (name, pairs) -> { name = name; pairs = pairs; })
+
+let ini: section_t list Parcoom.parser =
+  many section
+
+let () =
+  match Sys.argv |> Array.to_list with
+  | _ :: file_path :: _ ->
+     let result = file_path
+                  |> read_whole_file
+                  |> Parcoom.run ini
+     in
+     (match result with
+      | Ok sections -> sections |> show_sections |> print_endline
+      | Error error -> Printf.printf
+                         "Error during parsing at position %d: %s"
+                         error.pos
+                         error.desc)
+  | _ -> failwith "Expected path to an ini file"
